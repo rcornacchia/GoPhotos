4ae03216a496dd3a2d4cbd4fe23bb25d853f48bd
Rob Cornacchia <robcorn@uber.com>
2018-12-01 16:41:01 -0500

    added http servemux

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handlerFunc)
+	// http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", mux)
 }
