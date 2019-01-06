936872d5ec015a463e060c01bb9a1c223f4f326c
Rob Cornacchia <robcorn@uber.com>
2019-01-06 16:01:07 -0500

    update routes to respond to specific HTTP calls and added Create method to User controller

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"lenslocked.com/views"
@@ -22,8 +23,15 @@ type Users struct {
 }
 
 // New method for rendering the signup view
+// GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+// Create is used to process the signup form
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a temporary response.")
 }
